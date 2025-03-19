3d79845ef3dc063154776d7434a8703df1c34866
juur <juur@users.noreply.github.com>
2025-03-19 22:03:25 +0000

    changes

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -31,12 +31,12 @@
 #define S_IWOTH		   0002
 #define S_IXOTH		   0001
 
-#define S_ISDIR(mode) (((mode) & S_IFMT) == S_IFDIR)
-#define S_ISLNK(mode) (((mode) & S_IFMT) == S_IFLNK)
-#define S_ISBLK(mode) (((mode) & S_IFMT) == S_IFBLK)
-#define S_ISCHR(mode) (((mode) & S_IFMT) == S_IFCHR)
+#define S_ISDIR(mode)  (((mode) & S_IFMT) == S_IFDIR)
+#define S_ISLNK(mode)  (((mode) & S_IFMT) == S_IFLNK)
+#define S_ISBLK(mode)  (((mode) & S_IFMT) == S_IFBLK)
+#define S_ISCHR(mode)  (((mode) & S_IFMT) == S_IFCHR)
 #define S_ISFIFO(mode) (((mode) & S_IFMT) == S_IFIFO)
-#define S_ISREG(mode) (((mode) & S_IFMT) == S_IFREG)
+#define S_ISREG(mode)  (((mode) & S_IFMT) == S_IFREG)
 #define S_ISSOCK(mode) (((mode) & S_IFMT) == S_IFSOCK)
 
 struct stat {
