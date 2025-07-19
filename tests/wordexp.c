a7b162293fa0b779f8ab663c6cd05a94eed07fa0
juur <juur@users.noreply.github.com>
2025-07-19 15:52:58 +0100

    changes

---
+++ tests/wordexp.c
@@ -0,0 +1,30 @@
+#define _XOPEN_SOURCE 700
+
+#include <wordexp.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    setlocale(LC_ALL, "");
+
+    if (argc != 2)
+        exit(EXIT_FAILURE);
+
+    wordexp_t we;
+
+    memset(&we, 0, sizeof(we));
+
+    int rc = wordexp(argv[1], &we, 0);
+
+    printf("rc:           %d\n", rc);
+    printf("we_wordc:     %ld\n", we.we_wordc);
+    printf("we_offs:      %ld\n", we.we_offs);
+
+    for (size_t i = 0; i < we.we_wordc; i++)
+        printf("we_wordv[%02ld]: '%s'\n", i, we.we_wordv[i]);
+
+    exit(EXIT_SUCCESS);
+}
